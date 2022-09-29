74c6e6096c5aac0b246ade64f12d743754a6418f
lucas <105653717+Todpig@users.noreply.github.com>
2022-09-29 20:22:33 -0300

    mais codigo

--- Fusion/core/views.py
+++ Fusion/core/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Cargo, Funcionario, Servico
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all() ##.order_by('?') faz embaralhar os dados apresentados no template
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
